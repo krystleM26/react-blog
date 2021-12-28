540df122901945b0b1e3a8fab87e07f7a9ba274d
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-12-28 11:16:53 -0800

    Update Create component

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,63 +1,20 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-const Create = () => {
 
-    const [ title, setTitle] = useState('');
-    const [body, setBody] = useState('')
-    const [ author, setAuthor] = useState('')
-    const [isPending, setIsPending] = useState('false')
-    const history = useHistory();
-
-    const handleSubmit= (e) => {
-        e.preventDefault();
-        const blog = { title, body, author};
-        
-        setIsPending(true);
-
-        fetch('https/localhost/8000/blogs', {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(blog)
-        }).then(() => {
-            setIsPending(false);
-            history.push('/')
-        })
-    }
 
     
 
   return (
     <div className="create">
-        <h2>Add a New Blog</h2>
-        <form onSubmit={handleSubmit}>
-            <label>Blog Title</label>
-            <input
-            type="text"
-            required
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            />
-
-            <label>Blog Body:</label>
-            <textarea
-                required
-                value={body}
-                onChange={(e) => setBody(e.target.value)}
-            />
-
-            <label>Blog Author</label>
-            <select
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            >
-            <option value="Shubham">Shubham</option>
-                    <option value="Satyam">Satyam</option>
-                    <option value="Anmol">Anmol</option>
-
-            </select>
-            {!isPending && <button> Add Blog</button>}
-            {isPending && <button disabled> Adding Blog</button>}
+        <form className="createForm">
+            <div className="createFormGroup">
+                <label htmlFor="fileInput">
+                    <i class="fas fa-plus"></i>
+                </label>
+                <input type="file" id="fileInput" />
+                <input type="text" placeholder="Title" />
+            </div>
         </form>
       
     </div>
